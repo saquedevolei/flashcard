06fa6e26494267c1866981938a3a61826007ede3
saquedevolei <vinicius.correia21@escola.pr.gov.br>
2025-08-12 22:43:39 +0000

    Atualiza flashcards com correção do código

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+console.log("app.js carregou!");
+const perguntas = [
+    {
+        pergunta: "O que é JavaScript?",
+        resposta: "JavaScript é uma linguagem de programação usada para criar interatividade em sites."
+    },
+    {
+        pergunta: "O que significa HTML?",
+        resposta: "HTML significa HyperText Markup Language."
+    },
+    {
+        pergunta: "Para que serve o CSS?",
+        resposta: "O CSS é usado para estilizar e formatar páginas web."
+    }
+];
+
 const container = document.getElementById("container");
 
 perguntas.forEach(item => {
